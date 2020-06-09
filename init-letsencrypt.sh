0ad191c3801149eb227afa6b453369bfd924f093
Fabian <FBosler@gmail.com>
2020-06-09 11:03:27 +0200

    fixed lets encryt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(metaschule.com www.metaschule.de)
+domains=(metaschulapp.de www.metaschulapp.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="FBosler@gmail.com" # Adding a valid address is strongly recommended
