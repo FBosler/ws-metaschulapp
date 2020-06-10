c39d0f3d6d238c24621e0a8688f01c179d0038c1
Fabian <FBosler@gmail.com>
2020-06-10 08:32:49 +0200

    added app schema

--- backend/models/App.js
+++ backend/models/App.js
@@ -1,26 +1,46 @@
 import mongoose from "mongoose";
 
+const PLATFORMS = ["iOS", "Android", "Windows", "Mac OS", "Linux"];
+
+const BROWSERS = ["Chrome", "Firefox", "Safari", "Edge"];
+
+const APPTYPES = ["Web-Plattform", "native App", "Web-App", "YouTube-Channel", "Softwareprogramm"];
+
+const STORES = ["Apple App-Store", "Google Play"];
+
+const LICENSES = ["Einzellizenz", "offene Lizenz", "Schullizenz", "Klassenlizenz", "Fachschaftslizenz"];
+
 const SCHOOLTYPES = [
-    "Kindergarte",
-    "Grundschule",
-    "Hauptschule",
     "Gymnasium",
-    "Realschule",
     "Gesamtschule",
-    "Förderschule",
+    "Realschule",
+    "Schule mit mehreren Bildungsgängen",
+    "Hauptschule",
     "Berufliche Schule",
+    "Grundschule",
+    "Förderschule",
+    "Erwachsenenbildung",
+    "Kindergarten",
+];
+
+const CLASSES = [
+    "1./2. Schuljahr",
+    "3./4. Schuljahr",
+    "5./6. Schuljahr",
+    "9./10. Schuljahr",
+    "Sekundarstufe II",
+    "Lebenslanges Lernen",
 ];
 
 const SUBJECTS = [
-    "fachunabhängige Verwendung",
     "Arbeitslehre",
     "Astronomie",
     "Berufsvorbereitung",
     "Biologie",
     "Chemie",
     "Deutsch",
-    "Deutsch als Zweitsprache (DaZ)",
     "Deutsch als Fremdsprache (DaF)",
+    "Deutsch als Zweitsprache (DaZ)",
     "Englisch",
     "Erdkunde/Geografie",
     "Ethik",
@@ -36,10 +56,10 @@ const SUBJECTS = [
     "Mathematik",
     "Musik",
     "Naturwissenschaften",
-    "Pädagogik",
     "Physik",
     "Politik",
     "Praktische Philosophie",
+    "Pädagogik",
     "Russisch",
     "Sachunterricht",
     "Sozialwissenschaften",
@@ -47,49 +67,72 @@ const SUBJECTS = [
     "Sprachen",
     "Technik",
     "Wirtschaft",
-    "Alle",
+    "fachunabhängige Verwendung",
 ];
 
 const USECASE = [
     "Organisation/Administration",
+    "Infrastruktur",
+    "Nachhilfe",
+    "Selbstlernen",
     "Unterrichtsvorbereitung",
     "Unterrichtsdurchführung",
-    "Selbstlernen",
     "Fortbildung für Lehrer*innen",
-    "Infrastruktur",
-    "Alle",
 ];
 
-const SubjectSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        enum: SUBJECTS,
-    },
-    coveredGrades: {
-        type: [String],
-        enum: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    },
-});
+const TEACHINGPHASES = [
+    "Anwenden",
+    "Einführen",
+    "Erarbeiten",
+    "Hausaufgaben",
+    "Produkt wird nicht im Unterricht eingesetzt",
+    "Testen",
+    "Üben",
+];
+
+const DIDACTICS = [
+    "Einzelarbeit",
+    "Gruppenarbeit",
+    "Inklusion",
+    "Projekt",
+    "Quiz",
+    "Spiel",
+    "Vertretungsstunde",
+    "flipped classroom",
+    "offener Unterricht",
+];
 
 const UserSchema = new mongoose.Schema(
     {
         createdAt: { type: Date, default: Date.now },
         name: { type: String },
+        provider: { type: String },
         url: { type: String },
         description: { type: String },
-        focusesOn: { type: [String], enum: ["Eltern", "Lehrer", "Schüler"] },
-        useCase: {type: [String], enum: USECASE},
-        applicableTo: {
-            subjects: {
-                type: [SubjectSchema],
-            },
-            schoolTypes: {
-                type: [String],
-                enum: SCHOOLTYPES,
-            },
-        },
-        nativeApp: { type: [String], enum: ["Iphone", "Android", "Ipad", "Mac", "PC"] },
+        focusesOn: { type: [String], enum: ["Eltern", "Lehrer", "Schüler", "Verwaltung"] },
+        realeasedIn: { type: Number },
+        supportedPlatforms: { type: [string], enum: PLATFORMS },
+        supportedBrowser: { type: [string], enum: BROWSERS },
         requiresInternet: { type: Boolean },
+        offlineModeAvailable: { type: Boolean },
+        comments: { type: String },
+        appTypes: { type: [string], enum: APPTYPES },
+        stores: { type: [string], enum: STORES },
+        licenses: { type: [string], enum: LICENSES },
+        prices: {
+            single: { type: String },
+            studentAssociation: { type: String },
+            class: { type: String },
+            school: { type: String },
+            other: { type: String },
+        },
+        schoolTypes: { type: [String], enum: SCHOOLTYPES },
+        classes: { type: [String], enum: CLASSES },
+        subjects: { type: [String], enum: [SUBJECTS] },
+        useCase: { type: [String], enum: USECASE },
+        teachingPhases: { type: [String], enum: TEACHINGPHASES },
+        didactics: {type: [String], enum: DIDACTICS},
+        nativeApp: { type: [String], enum: ["Iphone", "Android", "Ipad", "Mac", "PC"] },
         images: {},
     },
     { strict: false },
