ccc5915a926db9cf937184729e747796d2007ca6
timokruth <timo.kruth@gmx.de>
2020-07-06 20:47:30 +0200

    new font for better low res readability (alternative would be "Montserrat")

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -36,11 +36,7 @@ export default class MyDocument extends Document {
                     <meta name="description" content="One App to rule them all, One App to find them" />
                     <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                     <link rel="manifest" href="/manifest.json" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;900&display=swap"
-                        rel="stylesheet"
-                    />
-                    <script src="https://account.snatchbot.me/script.js"></script>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>                     <script src="https://account.snatchbot.me/script.js"></script>
                 </Head>
                 <body>
                     <Main />
